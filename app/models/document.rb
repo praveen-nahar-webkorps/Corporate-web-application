c09e4875d38922a4f6a133c1023a2cdf75adbef5
Praveen <praveen@webkorps.com>
2023-08-29 15:41:42 +0530

    space commit

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,7 +3,9 @@ class Document < ApplicationRecord
   DOC_TYPES = ['Marksheet', 'Photo ID', 'Address Proof', 'Others'].freeze
 
   belongs_to :employee
+  
   has_one_attached :img
+
   validates :name, :doc_type, presence: true
 
 end
